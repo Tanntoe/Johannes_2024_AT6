f975fe1c00532d5345b9bb36c5f74258ea5f00be
Johannes Framnes <johfra01508@stud.noroff.no>
2024-11-26 01:05:53 +0100

    Third commit

--- UserInputValidatorInstances.py
+++ UserInputValidatorInstances.py
@@ -5,3 +5,9 @@ print(validation1.validate_positive_integers())
 
 validation2 = uiv.UserInputValidator(["66", "pizza", "-1", "1337"])
 print(validation2.validate_positive_integers())
+
+#Did not follow the instructions well enough and called the initiations in the previous commit. I will instead commit this comment and
+#a new instance.
+
+validation3 = uiv.UserInputValidator(["1", "ring", "2", "rule", "them", ":-1"])
+print(validation3.validate_positive_integers())
